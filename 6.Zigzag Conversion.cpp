4d53c5f7ae687db972c14afadb80f6c9eccee75c
Maverick7728 <manan.khandelwal.22cse@bmu.edu.in>
2024-09-29 00:35:36 +0530

    Update 6.Zigzag Conversion.cpp

--- 6.Zigzag Conversion.cpp
+++ 6.Zigzag Conversion.cpp
@@ -3,7 +3,8 @@
 // A P L S I I G
 // Y   I   R
 // And then read line by line: "PAHNAPLSIIGYIR"
-// Write the code that will take a string and make this conversion given a number of rows: string convert(string s, int numRows);
+// Write the code that will take a string and make this conversion given a number of rows: 
+//    string convert(string s, int numRows);
 
 // Example 1:
 // Input: s = "PAYPALISHIRING", numRows = 3
