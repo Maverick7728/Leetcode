503d3405f081f088c632e1b1b0c0ffbaa136af9f
Maverick7728 <manan.khandelwal.22cse@bmu.edu.in>
2025-06-11 22:10:38 +0530

    question number 20

---
+++ 20. Merge two sorted list.cpp
@@ -0,0 +1,62 @@
+// You are given the heads of two sorted linked lists list1 and list2.
+// Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+// Return the head of the merged linked list.
+
+
+// Example 1:
+
+// Input: list1 = [1,2,4], list2 = [1,3,4]
+// Output: [1,1,2,3,4,4]
+// Example 2:
+
+// Input: list1 = [], list2 = []
+// Output: []
+// Example 3:
+
+// Input: list1 = [], list2 = [0]
+// Output: [0]
+ 
+
+// Constraints:
+
+// The number of nodes in both lists is in the range [0, 50].
+// -100 <= Node.val <= 100
+// Both list1 and list2 are sorted in non-decreasing order.
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* curr1 = list1;
+        ListNode* curr2 = list2;
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* temp = dummyNode;
+
+        while(curr1 && curr2) {
+            if(curr1->val < curr2->val) {
+                temp->next = curr1;
+                temp = curr1;
+                curr1 = curr1->next;
+            }
+            else {
+                temp->next = curr2;
+                temp = curr2;
+                curr2 = curr2->next;
+            }
+        }
+        if(curr1) temp->next = curr1;
+        if(curr2) temp->next = curr2;
+        return dummyNode->next;
+    }
+};
